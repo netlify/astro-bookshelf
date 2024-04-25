457b0ca1fe18ee9f59e41866bcd9bb99815dc294
Matt Kane <matt.kane@netlify.com>
2024-04-25 09:12:17 +0100

    Use type tag

--- src/pages/api/webhook.json.ts
+++ src/pages/api/webhook.json.ts
@@ -3,7 +3,7 @@ import { purgeCache } from '@netlify/functions'
 export async function POST({ request }:any) {
   const body = await request.json()
 
-  await purgeCache({ tags: [body.sys.id] }).catch((error) => {
+  await purgeCache({ tags: [body.sys.id, "books"] }).catch((error) => {
     return new Response(JSON.stringify({
       message: `Failed to revalidate entry with id ${body.sys.id}`,
       error: error.message,
